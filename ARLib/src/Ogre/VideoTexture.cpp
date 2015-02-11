c119ea80d7cfc0d0405ef2715148915373d7031c
ands <amanti@gmx.de>
2015-02-11 13:09:05 +0100

    implemented homography transformation and merged it into the existing coordinate mapping, removed some hardcoded values

--- ARLib/src/Ogre/VideoTexture.cpp
+++ ARLib/src/Ogre/VideoTexture.cpp
@@ -10,26 +10,26 @@ namespace ARLib {
 			textureName,
 			Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
 			Ogre::TEX_TYPE_2D,
-			1280, 960,
+			player.getVideoWidth(), player.getVideoHeight(),
 			0,
 			Ogre::PF_BYTE_BGR,
 			Ogre::TU_DYNAMIC_WRITE_ONLY_DISCARDABLE);
 
 		// load undistortion texture
-		float *undistortionMapXY = new float[2 * 1280 * 960];
+		float *undistortionMapXY = new float[2 * player.getVideoWidth() * player.getVideoHeight()];
 		if (undistortionMapXY)
 		{
 			undistortionMapTexture = Ogre::TextureManager::getSingleton().createManual(
 				undistortionMapTextureName,
 				Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
 				Ogre::TEX_TYPE_2D,
-				1280, 960,
+				player.getVideoWidth(), player.getVideoHeight(),
 				0,
 				Ogre::PF_FLOAT16_GR,
 				Ogre::TU_STATIC_WRITE_ONLY);
 
 			player.calculateUndistortionMap(undistortionMapXY);
-			Ogre::PixelBox pb(1280, 960, 1, Ogre::PF_FLOAT32_GR, undistortionMapXY);
+			Ogre::PixelBox pb(player.getVideoWidth(), player.getVideoHeight(), 1, Ogre::PF_FLOAT32_GR, undistortionMapXY);
 			undistortionMapTexture->getBuffer()->blitFromMemory(pb);
 			delete[] undistortionMapXY;
 		}
@@ -44,6 +44,6 @@ namespace ARLib {
 	{
 		void *data = player.update();
 		if(data)
-			texture->getBuffer()->blitFromMemory(Ogre::PixelBox(1280, 960, 1, Ogre::PF_BYTE_BGR, data));
+			texture->getBuffer()->blitFromMemory(Ogre::PixelBox(player.getVideoWidth(), player.getVideoHeight(), 1, Ogre::PF_BYTE_BGR, data));
 	}
 }; // ARLib namespace
