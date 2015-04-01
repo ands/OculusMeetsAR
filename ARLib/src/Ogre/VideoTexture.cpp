c58d2e58337e8e704ddbf823e9ae5f567bb3bbe3
ands <amanti@gmx.de>
2015-04-02 01:40:59 +0200

    proper texture initialization and deletion, enabled vsync

--- ARLib/src/Ogre/VideoTexture.cpp
+++ ARLib/src/Ogre/VideoTexture.cpp
@@ -7,14 +7,24 @@ namespace ARLib {
 	VideoTexture::VideoTexture(VideoPlayer *_player, const Ogre::String& textureName, const Ogre::String& undistortionMapTextureName)
 		: player(_player)
 	{
-		texture = Ogre::TextureManager::getSingleton().createManual(
-			textureName,
-			Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
-			Ogre::TEX_TYPE_2D,
-			player->getVideoWidth(), player->getVideoHeight(),
-			0,
-			Ogre::PF_BYTE_BGR,
-			Ogre::TU_DYNAMIC_WRITE_ONLY_DISCARDABLE);
+		// initialize video texture
+		unsigned char *empty = new unsigned char[3 * player->getVideoWidth() * player->getVideoHeight()];
+		if (empty)
+		{
+			texture = Ogre::TextureManager::getSingleton().createManual(
+				textureName,
+				Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+				Ogre::TEX_TYPE_2D,
+				player->getVideoWidth(), player->getVideoHeight(),
+				0,
+				Ogre::PF_BYTE_BGR,
+				Ogre::TU_DYNAMIC_WRITE_ONLY_DISCARDABLE);
+
+			memset(empty, 0, 3 * player->getVideoWidth() * player->getVideoHeight());
+			Ogre::PixelBox pb(player->getVideoWidth(), player->getVideoHeight(), 1, Ogre::PF_BYTE_BGR, empty);
+			texture->getBuffer()->blitFromMemory(pb);
+			delete[] empty;
+		}
 
 		// load undistortion texture
 		float *undistortionMapXY = new float[2 * player->getVideoWidth() * player->getVideoHeight()];
@@ -38,7 +48,8 @@ namespace ARLib {
 
 	VideoTexture::~VideoTexture()
 	{
-		// TODO: release textures?
+		Ogre::TextureManager::getSingleton().remove(texture->getHandle());
+		Ogre::TextureManager::getSingleton().remove(undistortionMapTexture->getHandle());
 	}
 
 	bool VideoTexture::update(LARGE_INTEGER *captureTimeStamp)
